278d04f90c02909cfd40237ee2a3d95431a7a3b4
davidabutler92 <davidabutler92@yahoo.com>
2020-08-18 11:36:59 -0700

    add store users input in a vairabe. devide countTo by count By

--- js/main.js
+++ js/main.js
@@ -1,13 +1,11 @@
-function countToBy(countTo, countBy) {
-  for (let i= 0; i < countTo; i += countBy) {
-    $("#results").append(i + " ");
-  }
-}
-
 $(document).ready(function() {
   $("#counter-form").submit(function(event) {
     event.preventDefault();
-    const countTo = parseInt($("#countTo").val());
-    console.log(countTo);
+    const countTo = parseInt($("input#countTo").val());
+    const countBy = parseInt($("input#countBy").val());
+    
+    const arraySize = (countTo/countBy);
+    arraySize = Math.floor(arrayOfNumbers);
+
   });
 });
